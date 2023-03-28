44ba3d7109df82ab5731e5858e031d49997baa80
Samuel Majerus <ladolego@gmail.com>
2023-03-27 17:21:18 -0700

    Linked-List class logic, Test 2 -- Pass.   (Lsn 2)

--- __tests__/linked-list.test.js
+++ __tests__/linked-list.test.js
@@ -6,4 +6,9 @@ describe('LinkedList', () => {
     let linkedList = new LinkedList();
     expect(linkedList.head).toEqual(null);  //If Linked-List is initialized, the 'head' field is 'null'.
   });
+  test('LinkedList.prototype.insertLast() should add a node at head if a linked list has no head', () => {
+    let linkedList = new LinkedList();
+    linkedList.insertLast("head");
+    expect(linkedList.head.data).toEqual("head");
+  });
 }); 
